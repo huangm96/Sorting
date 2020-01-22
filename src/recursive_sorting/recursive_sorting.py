9b12e2fe2820fae351390001aef4b675b3117838
Min-Huang <minh.huang96@gmail.com>
2020-01-22 12:14:40 -0800

    finished searching

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -16,21 +16,17 @@ def merge( arrA, arrB ):
             for n in range(a_index+b_index, len(merged_arr)):
                 merged_arr[n] = arrB[b_index]
                 b_index += 1
-
         elif b_index == len(arrB):
             for n in range(a_index+b_index, len(merged_arr)):
                 merged_arr[n] = arrA[a_index]
                 a_index += 1
-
         # find the smaller number, put the smaller number in the merged_arr, and the index of the smaller number array +1    
         elif arrA[a_index] >= arrB[b_index]:
             merged_arr[i] = arrB[b_index]
             b_index += 1
-
         elif arrA[a_index] < arrB[b_index]:
             merged_arr[i] = arrA[a_index]
             a_index += 1
-
     print("merged_arr", merged_arr)        
     return merged_arr
 
@@ -39,11 +35,11 @@ def merge( arrA, arrB ):
 def merge_sort( arr ):
     # TO-DO
     print(arr)
-    if len(arr) > 1:
-        
-        left = merge_sort(arr[0: len(arr) // 2])
-        right = merge_sort(arr[len(arr) // 2 :])
-        arr = merge(left, right)
+    if len(arr) <= 1:
+        return arr
+    left = merge_sort(arr[0: len(arr) // 2])
+    right = merge_sort(arr[len(arr) // 2 :])
+    arr = merge(left, right)
     return arr
 
 
@@ -66,20 +62,4 @@ def timsort( arr ):
     return arr
 
 
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
-    print(len(merged_arr))
-    # TO-DO
-    arr_a_index = 0
-    arr_b_index = 0
-    for i in range(0, len(merged_arr)):
-        print("i", i)
-        print("merged", merged_arr)
-        if arrA[arr_a_index] < arrB[arr_b_index]:
-            merged_arr[i] = arrA[arr_a_index]
-            arr_a_index += 1
-        else:
-            merged_arr[i] = arrB[arr_b_index]
-            arr_b_index += 1
-    return merged_arr
+
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -3,7 +3,10 @@ def linear_search(arr, target):
   
   # TO-DO: add missing code
 
-   return -1   # not found
+  for i in range(len(arr)):
+    if arr[i] == target:
+      return i
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -16,7 +19,15 @@ def binary_search(arr, target):
   high = len(arr)-1
 
   # TO-DO: add missing code
+  while low<=high:
+    middle_index = (low + high) // 2
 
+    if arr[middle_index] == target:
+      return middle_index
+    elif arr[middle_index] > target:
+      high = middle_index - 1
+    elif arr[middle_index] < target:
+      low = middle_index +1
   return -1 # not found
 
 
@@ -28,3 +39,11 @@ def binary_search_recursive(arr, target, low, high):
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+  elif low > high:
+    return -1
+  elif arr[middle] == target:
+    return middle
+  elif arr[middle] > target:
+    return binary_search_recursive(arr, target, low, middle-1)
+  elif arr[middle] < target:
+    return binary_search_recursive(arr, target, middle+1, high)
